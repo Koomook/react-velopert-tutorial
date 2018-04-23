8b7428cb5df2659e2c54760c470a39ed62fa84ca
koomook <bongbonggg97@gmail.com>
2018-04-24 01:56:47 +0900

    Add default props for onRemove : give it to PhoneInfo

--- phone-book/src/components/PhoneInfoList.js
+++ phone-book/src/components/PhoneInfoList.js
@@ -1,21 +1,29 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PhoneInfo from './PhoneInfo';
 
 class PhoneInfoList extends Component {
   static defaultProps = {
-    data: []
-  };
-  render(){
-    const {data} = this.props;
+    list: [],
+    onRemove: () => console.warn('onRemove not defined'),
+  }
+
+  render() {
+    const { data, onRemove } = this.props;
     const list = data.map(
-      info => (<PhoneInfo key={info.id} info={info}/>)
+      info => (
+        <PhoneInfo
+          key={info.id}
+          info={info}
+          onRemove={onRemove}
+        />)
     );
 
     return (
       <div>
-        {list}
+        {list}    
       </div>
-    )
+    );
   }
 }
+
 export default PhoneInfoList;
