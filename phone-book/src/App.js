085cf17e2b0a8db0e82cad972f3e3916f454ffdc
koomook <bongbonggg97@gmail.com>
2018-04-24 01:55:25 +0900

    Add handleRemove : give it to PhoneInfoList

--- phone-book/src/App.js
+++ phone-book/src/App.js
@@ -24,6 +24,12 @@ class App extends Component {
       information: information.concat({id: this.id++, ...data})      
     });
   }
+  handleRemove = (id) => {
+    const {information} = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
   render() {
     const {information} = this.state;
     return (
@@ -31,7 +37,10 @@ class App extends Component {
         <PhoneForm
           onCreate={this.handleCreate}
         />
-        <PhoneInfoList data={this.state.information}/>
+        <PhoneInfoList 
+          data={information}
+          onRemove={this.handleRemove}
+          />
       </div>
     )
   }
