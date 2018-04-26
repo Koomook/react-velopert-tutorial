8bdc355faffe012375a142c884646a4d4610e6c4
koomook <bongbonggg97@gmail.com>
2018-04-26 23:28:08 +0900

    Add shouldComponentUpdate

--- phone-book/src/components/PhoneInfoList.js
+++ phone-book/src/components/PhoneInfoList.js
@@ -4,27 +4,24 @@ import PhoneInfo from './PhoneInfo';
 class PhoneInfoList extends Component {
   static defaultProps = {
     data: [],
-    onRemove: () => console.warn('onRemove not defined'),
-    onUpdate: () => console.warn('onUpdate not defined')
+    onRemove: () => console.warn("onRemove not defined"),
+    onUpdate: () => console.warn("onUpdate not defined")
   };
-  render(){
-    console.log(this.props)
-    const {data, onRemove, onUpdate} = this.props;
-    const list = data.map(
-      info => (
-        <PhoneInfo 
-          key={info.id} 
-          info={info}
-          onRemove={onRemove}
-          onUpdate={onUpdate}
-        />)
-    );
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.data !== this.props.data
+  }
+  render() {
+    const { data, onRemove, onUpdate } = this.props;
+    const list = data.map(info => (
+      <PhoneInfo
+        key={info.id}
+        info={info}
+        onRemove={onRemove}
+        onUpdate={onUpdate}
+      />
+    ));
 
-    return (
-      <div>
-        {list}    
-      </div>
-    );
+    return <div>{list}</div>;
   }
 }
 
